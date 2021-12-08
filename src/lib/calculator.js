9b96004f79530e115e29b158405b3300b88a431b
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2021-12-07 21:02:57 -0300

    Implementing other basic methods

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -8,3 +8,36 @@ module.exports.sum = (a, b) => {
 
   return number1 + number2;
 };
+
+module.exports.subtract = (a, b) => {
+  const number1 = parseInt(a, 10);
+  const number2 = parseInt(b, 10);
+
+  if (Number.isNaN(number1) || Number.isNaN(number2)) {
+    throw new Error('Please check your input');
+  }
+
+  return number1 - number2;
+}
+
+module.exports.multiply = (a, b) => {
+  const number1 = parseInt(a, 10);
+  const number2 = parseInt(b, 10);
+
+  if (Number.isNaN(number1) || Number.isNaN(number2)) {
+    throw new Error('Please check your input');
+  }
+
+  return number1 * number2;
+}
+
+module.exports.divide = (a, b) => {
+  const number1 = parseInt(a, 10);
+  const number2 = parseInt(b, 10);
+
+  if (Number.isNaN(number1) || Number.isNaN(number2)) {
+    throw new Error('Please check your input');
+  }
+
+  return number1 / number2;
+}
--- src/lib/calculator.test.js
+++ src/lib/calculator.test.js
@@ -1,7 +1,7 @@
-const { sum } = require('./calculator');
+const { sum, subtract, multiply, divide } = require('./calculator');
 
 it('should sum 2 and 2 and the result must be 4', () => {
-  expect(sum([2, 2], 2)).toBe(4);
+  expect(sum(2, 2)).toBe(4);
 });
 
 it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
@@ -23,5 +23,83 @@ it('should throw an error if arguments can not be summed', () => {
 
   expect(() => {
     sum()
+  }).toThrowError();
+});
+
+it('should subtract 2 from 6 and the result must be 4', () => {
+  expect(subtract(6, 2)).toBe(4);
+});
+
+it('should subtract 2 from 5 even if one of them is a string and the result must be 3', () => {
+  expect(subtract(5, '2')).toBe(3);
+});
+
+it('should throw an error if arguments can not be subtracted', () => {
+  expect(() => {
+    subtract('', 2)
+  }).toThrowError();
+
+  expect(() => {
+    subtract([1, 3])
+  }).toThrowError();
+
+  expect(() => {
+    subtract({})
+  }).toThrowError();
+
+  expect(() => {
+    subtract()
+  }).toThrowError();
+});
+
+it('should multiply 2 and 3 and the result must be 6', () => {
+  expect(multiply(3, 2)).toBe(6);
+});
+
+it('should multiply 2 and 3 even if one of them is a string and the result must be 6', () => {
+  expect(multiply('2', '3')).toBe(6);
+});
+
+it('should throw an error if arguments can not be multiplied', () => {
+  expect(() => {
+    multiply('', 2)
+  }).toThrowError();
+
+  expect(() => {
+    multiply([1, 3])
+  }).toThrowError();
+
+  expect(() => {
+    multiply({})
+  }).toThrowError();
+
+  expect(() => {
+    multiply()
+  }).toThrowError();
+});
+
+it('should divide 8 by 2 and the result must be 4', () => {
+  expect(divide(8, 2)).toBe(4);
+});
+
+it('should divide 8 and 2 even if one of them is a string and the result must be 4', () => {
+  expect(divide('8', '2')).toBe(4);
+});
+
+it('should throw an error if arguments can not be divided', () => {
+  expect(() => {
+    divide('', 2)
+  }).toThrowError();
+
+  expect(() => {
+    divide([1, 3])
+  }).toThrowError();
+
+  expect(() => {
+    divide({})
+  }).toThrowError();
+
+  expect(() => {
+    divide()
   }).toThrowError();
 });
