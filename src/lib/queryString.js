14aac19a16765f681d4839335dd1a25f86f29bac
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2021-12-07 23:27:21 -0300

    Added more tests to queryString module

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,2 +1,7 @@
 module.exports.queryString = (obj) =>
-  Object.keys(obj).map((key) => `${key}=${obj[key]}`).join('&');
+  Object.keys(obj).map((key) => {
+    if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+      throw new Error('Please, check your params');
+    }
+    return `${key}=${obj[key]}`
+  }).join('&');
--- src/lib/queryString.test.js
+++ src/lib/queryString.test.js
@@ -9,4 +9,28 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=John&profession=developer');
   });
+
+  it('should create a query string even if an array is passed as value', () => {
+    const data = {
+      name: 'John',
+      abilities: ['JS', 'TS', 'TDD'],
+    };
+
+    expect(queryString(data)).toBe('name=John&abilities=JS,TS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const data = {
+      name: 'John',
+      abilities: {
+        first: 'JS',
+        second: 'TS',
+        third: 'TDD'
+      },
+    };
+
+    expect(() => {
+      queryString(data);
+    }).toThrowError();
+  });
 });
